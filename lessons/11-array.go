eee3dac36c08c25dfc8fb0fe2fff4b12387985a6
Victor Tarasov <v-v-tarasov@github.com>
2020-05-12 13:02:24 +0300

    lesson Go

--- lessons/11-array.go
+++ lessons/11-array.go
@@ -5,9 +5,8 @@ package main
 import "fmt"
 
 func main() {
-	var xArray [5] uint32
+	var xArray [5]uint32
 	xArray[4] = 100
 	fmt.Println(xArray)
 	fmt.Println(xArray[4])
 }
-
--- lessons/13-slices.go
+++ lessons/13-slices.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	var x []float32
-
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
 }
-
