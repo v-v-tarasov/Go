e45a078561914cfde7bbe81871bdb1a11e34a1a9
Victor Tarasov <v.v.tarasov@gmail.com>
2020-05-15 12:56:54 +0300

    lesson Go

--- lessons/22-pointers.go
+++ lessons/22-pointers.go
@@ -8,8 +8,15 @@ func zero(xPtr *int) {
     *xPtr = 0
 }
 
+func nonZero(y int) {
+    y = 25
+}
+
 func main() {
     x := 5
+    y := 45
     zero(&x)
     fmt.Println(x)
+    nonZero(y)
+    fmt.Println(y)
 }
