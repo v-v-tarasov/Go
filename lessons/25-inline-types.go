e779e63e51dc249f7de70606b40db70e02e03b79
Victor Tarasov <v-v-tarasov@github.com>
2020-05-15 22:03:27 +0300

    lesson Go

--- lessons/25-inline-types.go
+++ lessons/25-inline-types.go
@@ -1,32 +1,9 @@
-// Встраиваемые типы ?или композиция?
+// Встраиваемые типы - Разобратся!!!
 
 package main
 
 import "fmt"
 
-<<<<<<< HEAD
-type Person struct {
-    Name string
-}
-
-func (p *Person) Introduce() {
-    fmt.Println("Hi, my name is", p.Name)
-}
-
-type Say struct {
-    *Person
-    Power int
-=======
 func main() {
-	fmt.Println("Start program...")
->>>>>>> 05f3fd459d44e7dcc157fc400fb8bf762335e6f3
+	fmt.Println("Разобраться в вопросе!")
 }
-
-func main() {
-    goku := &Say{
-        Person: &Person{"Goku"},
-        Power: 9001,
-    }
-    goku.Introduce()
-}
-
